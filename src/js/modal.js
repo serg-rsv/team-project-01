1ad65edc0102c67d2a56d8cc7241c04091f692e9
Nikolay-Tymoshchuk <saintnikfreak@gmail.com>
2022-01-31 10:32:29 +0200

    fix modal in garllery

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[modal-open-data]'),
+    closeModalBtn: document.querySelector('[modal-close-data]'),
+    modal: document.querySelector('[modal-data]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
