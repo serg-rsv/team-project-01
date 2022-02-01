faaf6f2c666ecc37495c6e6ea4dded74813994a1
serg-rsv <88489822+serg-rsv@users.noreply.github.com>
2022-02-01 19:51:13 +0000

    Deploying to gh-pages from @ serg-rsv/team-project-01@f84ce33a210fea9219279c8944c8e952e3866a03 🚀

--- src.bdb5cfb7.js
+++ src.bdb5cfb7.js
@@ -7,4 +7,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/mobile-menu"),require("./js/lib/modal-multy");
 },{"./sass/main.scss":"clu1","./js/mobile-menu":"sByI","./js/lib/modal-multy":"Jx8J"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-01/src.f0a20094.js.map
+//# sourceMappingURL=/team-project-01/src.bdb5cfb7.js.map
