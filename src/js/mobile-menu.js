4549ae16640cfda8ffd1ee8232477b9a76a79f74
Serhii Rybak <rybaksw@gmail.com>
2022-02-02 14:35:31 +0200

    fix menu js
    закрытие меню при переходе по ссылке

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,10 +3,12 @@
     openMenuBtn: document.querySelector('[data-menu-btn-open]'),
     closeMenuBtn: document.querySelector('[data-menu-btn-close]'),
     mobileMenu: document.querySelector('[data-menu]'),
+    mobileLink: document.querySelectorAll('[js-menu-link]'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.mobileLink.forEach(item => item.addEventListener('click', toggleModal));
 
   function toggleModal() {
     refs.mobileMenu.classList.toggle('is-hidden');
